a78b231bbfe4e4d673de5ab31eee88d2604ecdd8
Hyder S M <hysm@microsoft.com>
2019-02-18 23:36:06 -0800

    temp

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
-    $(".accordion").accordion({
+    var sliders = $(".lightSlider").lightSlider({
+        item:3,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        adaptiveHeight: true,
+        nextHtml: '<img style="width:110%" src="assets/images/rightArrow.png"/>',
+        prevHtml: '<img style="width:110%" src="assets/images/leftArrow.png"/>',
+    });
+
+    var accordion = $(".accordion").accordion({
         heightStyle: "content",
         autoHeight: false,
         collapsible: true,
-        active: false,
+        active: 0,
+        activate: function(event, ui) {
+            sliders.resize();
+        },
     });
+
     $(".accordion2").accordion({
         heightStyle: "content",
         autoHeight: false,
         collapsible: true,
         active: false,
     });
-    $("#lightSlider").lightSlider({
-        item:3,
-        loop:true,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:600,
-        auto:true,
-        pauseOnHover: true,
-    }); 
 });
